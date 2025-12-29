54421c94e27cc19bdab9074122da29b2a636d1f6
H4S0 <hasanalic10@gmail.com>
2025-12-29 16:26:09 +0100

    feat: add new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Ubuntu Mono"', 'ui-sans-serif', 'system-ui'],
+        mono: ['"Ubuntu Mono"', 'ui-monospace', 'SFMono-Regular'],
+      },
+    },
   },
+
   plugins: [],
 };
